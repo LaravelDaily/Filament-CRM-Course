cc7486618e951b8baac4fe9e1e3ce1e0978c7af5
modestasv <info@modestasv.lt>
2023-11-03 08:31:04 +0200

    Installed DebugBar, fixed n+1 issue in customers table

--- app/Filament/Resources/CustomerResource.php
+++ app/Filament/Resources/CustomerResource.php
@@ -118,6 +118,9 @@ class CustomerResource extends Resource
     public static function table(Table $table): Table
     {
         return $table
+            ->modifyQueryUsing(function ($query) {
+                return $query->with('tags');
+            })
             ->columns([
                 Tables\Columns\TextColumn::make('employee.name')
                     ->hidden(!auth()->user()->isAdmin()),
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -91,9 +91,14 @@ class DatabaseSeeder extends Seeder
         }
 
         $defaultPipelineStage = PipelineStage::where('is_default', true)->first()->id;
-        Customer::factory()->count(10)->create([
-            'pipeline_stage_id' => $defaultPipelineStage,
-        ]);
+        Customer::factory()
+            ->count(10)
+            ->create([
+                'pipeline_stage_id' => $defaultPipelineStage,
+            ])
+            ->each(function (Customer $customer) {
+                $customer->tags()->attach(random_int(1, 2));
+            });
 
         $customFields = [
             'Birth Date',
